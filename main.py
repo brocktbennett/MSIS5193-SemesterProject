81e788483fc8e424a2a302990f4cbb225b732647
Brock T. Bennett <brock.bennett@okstate.edu>
2023-09-12 12:32:01 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
+# Project Title: Semester Long Project
+# Course: MSIS 5193
+# Team Members:
+# - John Ramirez
+# - Brock Bennett
+# - Yulun Wang, PhD
+# - Reina Han
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Pseudo-code for Project
 
+# 1. Import Libraries
+# --------------------------------------
+# import necessary libraries such as NumPy, pandas, matplotlib, etc.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# 2. Initialize Variables and Configuration Settings
+# --------------------------------------
+# set configurations such as file paths, parameters, etc.
 
+# 3. Data Collection
+# --------------------------------------
+# def collect_data():
+#     if web scraping:
+#         # get data from websites
+#     elif API:
+#         # fetch data from API
+#     elif databases:
+#         # pull data from databases
+#     return collected_data
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 4. Data Preprocessing
+# --------------------------------------
+# def preprocess_data(raw_data):
+#     # remove duplicates
+#     # handle missing values
+#     # transform data types if necessary
+#     # normalize/scale data if needed
+#     return preprocessed_data
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 5. Exploratory Data Analysis (EDA)
+# --------------------------------------
+# def perform_eda(preprocessed_data):
+#     # generate descriptive statistics
+#     # plot graphs and distributions
+#     # identify patterns and correlations
+#     return insights
+
+# 6. Feature Engineering
+# --------------------------------------
+# def feature_engineering(preprocessed_data):
+#     # extract new features
+#     # select important features
+#     return final_features
+
+# 7. Model Training
+# --------------------------------------
+# def train_model(final_features):
+#     # split data into training and testing sets
+#     # select model
+#     # train model on training data
+#     # validate model on validation set
+#     return trained_model, performance_metrics
+
+# 8. Model Evaluation
+# --------------------------------------
+# def evaluate_model(trained_model, test_data):
+#     # test model
+#     # calculate performance metrics
+#     # plot evaluation metrics (if applicable)
+#     return evaluation_results
+
+# 9. Model Deployment (Optional)
+# --------------------------------------
+# def deploy_model(trained_model):
+#     # save the model
+#     # deploy the model to a server or API
+#     return deployment_status
